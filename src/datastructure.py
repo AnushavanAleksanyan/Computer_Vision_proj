da5c189862881c747618902de5a1c46dead712ef
A_Aleksanyan <a-a-2-0-0-7@yandex.ru>
2021-08-21 00:12:10 +0400

    004

--- src/datastructure.py
+++ src/datastructure.py
@@ -4,23 +4,34 @@ import os
 import shutil
 from zipfile import ZipFile
 from glob import glob
+import pathlib
 
 def acc():
-	ans = input("Type Y to unzip the archive: ")
+	ans = input("Type 'Y' to unzip the archive: ")
 	if ans.lower() == "y":
 		return True
 
+def rename_photos(fold):
+	path = pathlib.Path('.')/f"data/train/{fold}/"
+	for folder in path.iterdir():
+		if folder.is_dir():
+			counter = 0
+			for file in folder.iterdir():
+				if file.is_file():
+					new_file = folder.name + "_" + str(counter) + file.suffix
+					file.rename(path / folder.name / new_file)
+					counter += 1
+
 
 archive = "initial_data/500MB.zip"
-"""
+
 if acc():
 	with ZipFile(archive, 'r') as zip:
 	 	zip.extract("500MB/labels.csv", path ="data")
 
-if acc():
 	with ZipFile(archive) as zip_file:
 		zip_file.extractall("initial_data/")
-"""
+
 
 df = pd.read_csv("data/500MB/labels.csv")
 
@@ -31,22 +42,21 @@ folder_path = "data/train/"
 folder_list = glob("data/train/*/")
 f_names = ["Black", "Blue", "Green", "Pink", "Red", "White"]
 
-# for index, row in df.iterrows():
-# 	print (row['id'], row['Color'])
 files=os.listdir("initial_data/500MB/images/2302883/")
-"""
-
-for fname in files:
-	shutil.copy2(os.path.join("initial_data/500MB/images/2302883/",fname),"data/train/white/")
-"""
 
 def move(id, color):
+	i = 0
 	for address, dirs, files in os.walk("initial_data/500MB/images/"):
 		for dir in dirs:
 			files2 = os.listdir(f"initial_data/500MB/images/{id}/")
 			for fname in files2:
-				shutil.copy2(os.path.join(f"initial_data/500MB/images/{id}/",fname),f"data/train/{color}/")
-			os.rename(f"data/train/{color}/{fname}",f"data/train/{color}/{fname} - {dir}")
+				if i // 10 != 0:
+					dest = "train"
+				else:
+					dest = "test"
+				shutil.copy2(os.path.join(f"initial_data/500MB/images/{id}/",fname),f"data/{dest}/{color}/")
+			i+=1
+		rename_photos(color)
 
 for index, row in df.iterrows():
 	if not row['Color'] in f_names:
@@ -55,3 +65,8 @@ for index, row in df.iterrows():
 	else:
 		move(row['id'], row["Color"])
 		print(row['id'], row["Color"])
+
+
+
+if __name__ == "__main__":
+	rename_photos()
