0ef1980f093e60385c15827dc2a9617089d79997
A_Aleksanyan <a-a-2-0-0-7@yandex.ru>
2021-08-21 08:25:24 +0400

    004

--- src/datastructure.py
+++ src/datastructure.py
@@ -28,42 +28,39 @@ if acc():
 	with ZipFile(archive, 'r') as zip:
 	 	zip.extract("500MB/labels.csv", path ="data")
 
-	with ZipFile(archive) as zip_file:
-		zip_file.extractall("initial_data/")
+	# with ZipFile(archive) as zip_file:
+	# 	zip_file.extractall("initial_data/")
 
 
 df = pd.read_csv("data/500MB/labels.csv")
-
 # print(df.head(50))
 # print(df.columns)
 
-
 f_names = ["Black", "Blue", "Green", "Pink", "Red", "White"]
 
-def move(id, color):
-	i=0
+def move(id, color, dest):
 	for address, dirs, files in os.walk("initial_data/500MB/images/"):
-		i+=1
 		for dir in dirs:
-			if i % 10 != 0:
-				dest = "train"
-			else:
-				dest = "test"
-			print(dest)
 			files2 = os.listdir(f"initial_data/500MB/images/{id}/")
 			for fname in files2:
 				shutil.copy2(os.path.join(f"initial_data/500MB/images/{id}/",fname),f"data/{dest}/{color}/")
-		print (i)
 		rename_photos(color)
-			
-	
 
 
+i=0
 for index, row in df.iterrows():
-	if not row['Color'] in f_names:
-		move(row['id'], "other")
-		print(row['id'], "other")
+	if i%10 == 0:
+		if not row['Color'] in f_names:
+			move(row['id'], "other", "test")
+			print(row['id'], "other")
+		else:
+			move(row['id'], row["Color"], "test")
+			print(row['id'], row["Color"])
 	else:
-		move(row['id'], row["Color"])
-		print(row['id'], row["Color"])
+		if not row['Color'] in f_names:
+			move(row['id'], "other", "train")
+			print(row['id'], "other")
+		else:
+			move(row['id'], row["Color"], "train")
+			print(row['id'], row["Color"])
 
