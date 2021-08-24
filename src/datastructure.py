30688791b86a68c21ab795e6ba8c43fb6d2f2f07
A_Aleksanyan <a-a-2-0-0-7@yandex.ru>
2021-08-24 23:06:01 +0400

    changes

--- src/datastructure.py
+++ src/datastructure.py
@@ -4,63 +4,126 @@ import os
 import shutil
 from zipfile import ZipFile
 import pathlib
+from glob2 import glob
 
 def acc():
 	ans = input("Type 'Y' to unzip the archive: ")
 	if ans.lower() == "y":
 		return True
 
-def rename_photos(fold):
-	path = pathlib.Path('.')/f"data/train/{fold}/"
-	for folder in path.iterdir():
-		if folder.is_dir():
-			counter = 0
-			for file in folder.iterdir():
-				if file.is_file():
-					new_file = folder.name + "_" + str(counter) + file.suffix
-					file.rename(path / folder.name / new_file)
-					counter += 1
 
-
-archive = "initial_data/500MB.zip"
+archive = "500MB.zip"
 
 if acc():
+	#os.mkdir("initial_data")
 	with ZipFile(archive, 'r') as zip:
-	 	zip.extract("500MB/labels.csv", path ="data")
+	 	zip.extractall()
+	os.rename(os.path.abspath("500MB"), os.path.abspath("initial_data"))
 
-	# with ZipFile(archive) as zip_file:
-	# 	zip_file.extractall("initial_data/")
+def create_data_folders():
+	os.mkdir(os.path.abspath("data"))
+	os.mkdir(os.path.abspath("data/train"))
+	os.mkdir(os.path.abspath("data/test"))
+	os.mkdir(os.path.abspath("data/validation"))
+	return("created")
+
+# create_data_folders()
 
 
+def create_folders_for_features():
+	df = pd.read_csv("initial_data/labels.csv")
+	colors = df["Color"].unique()
+	for color in colors:
+		os.mkdir(os.path.abspath("data/train/"+color))
+		os.mkdir(os.path.abspath("data/test/"+color))
+		# os.mkdir(os.path.abspath("data/validation/"+color))
+	return("made!")
+
+# create_folders_for_features()
+
+
+df = pd.read_csv("initial_data/labels.csv")
+
+folders = [ name for name in os.listdir("initial_data/images/") if os.path.isdir(os.path.join("initial_data/images/", name)) ]
+
+def move_images():
+	for id, color in df[["id","Color"]].values:
+		files = [f for f in os.listdir(f"initial_data/images/{id}/") if os.path.isfile(os.path.join(f"initial_data/images/{id}/", f))]
+		for file in files:
+			print(file, id, color)
+			shutil.copy2(os.path.join(f"initial_data/images/{id}/",file),f"data/train/{color}/{str(id)+file}")
+
+
+# move_images()
+
+
+def devide_test():
+	for folder in os.listdir(f"data/train/"):
+		for file in os.listdir(f"data/train/{folder}/"):
+			print(file)
+
+
+devide_test()
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 df = pd.read_csv("data/500MB/labels.csv")
-# print(df.head(50))
-# print(df.columns)
 
-f_names = ["Black", "Blue", "Green", "Pink", "Red", "White"]
 
-def move(id, color, dest):
+
+def move(id, color):
 	for address, dirs, files in os.walk("initial_data/500MB/images/"):
 		for dir in dirs:
 			files2 = os.listdir(f"initial_data/500MB/images/{id}/")
 			for fname in files2:
-				shutil.copy2(os.path.join(f"initial_data/500MB/images/{id}/",fname),f"data/{dest}/{color}/")
+				shutil.copy2(os.path.join(f"initial_data/500MB/images/{id}/",fname),f"data/train/{color}/")
 		rename_photos(color)
 
 
-i=0
-for index, row in df.iterrows():
-	if i%10 == 0:
-		if not row['Color'] in f_names:
-			move(row['id'], "other", "test")
-			print(row['id'], "other")
-		else:
-			move(row['id'], row["Color"], "test")
-			print(row['id'], row["Color"])
-	else:
-		if not row['Color'] in f_names:
-			move(row['id'], "other", "train")
-			print(row['id'], "other")
-		else:
-			move(row['id'], row["Color"], "train")
-			print(row['id'], row["Color"])
+def arrange_img(var, id, color, file):
+	shutil.copy2(f"initial_data/500MB/images/{id}/{file}",f"data/{var}/{color}/{str(id)+file}")
 
+
+if __name__ == "__main__":
+	filelist = []
+	folderlist = []
+	i=0
+	for root,dirs,files in os.walk(f"initial_data/500MB/images/"):
+
+		print(os.listdir(root))
+			# for file in files:
+			# 	print(file)
+	# 		filelist.append(file)
+	# 		folderlist.append(root)
+	# # print(len(filelist))
+	# print(len(folderlist))
+	#print(dirlist)
+	# fold = iter(dirlist)
+	# if i%10 == 0:
+	# 	var = "test"
+	# else:
+	# 	var = "train"
+	# print(df[df['id']==next(fold)])
+	# if not df[df['id']==next(fold)]["Color"] in f_names:
+	# 	#arrange_img(var, folderlist[i], "other",filelist[i])
+	# 	print(var, folderlist[i], "other", filelist[i])
+	# else:
+	# 	#arrange_img(var, folderlist[i], row["Color"], filelist[i])
+	# 	print(var, folderlist[i], df[df['id']==next(fold)]["Color"], filelist[i])
+	# i += 1
+
+# result = df[df['id']==2462198]["Color"]
+# print(result)
+
+# for fname in filelist:
+"""
